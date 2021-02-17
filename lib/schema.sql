ed7bb112e4abed0a17dcc12d44d65ba54b27d610
Nana <72888369+nanaama527@users.noreply.github.com>
2021-02-17 18:49:31 -0500

    hi

---
+++ lib/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE series (
+id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE sub_genres (
+id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE authors (
+id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE books (
+id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE characters (
+id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE character_books (
+id INTEGER PRIMARY KEY
+
+);
