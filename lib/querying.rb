d8a3aadc1d7a73c785ec89fe6fe8b3651fb005d9
Nana <72888369+nanaama527@users.noreply.github.com>
2021-02-19 18:27:55 -0500

    done

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,28 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT bookS.title, bookS.year FROM BOOKS JOIN SERIES ON BOOKS.SERIES_ID=SERIES.ID 
+  WHERE SERIES.ID=1 ORDER BY YEAR;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM CHARACTERS ORDER BY LENGTH(MOTTO) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*) FROM CHARACTERS GROUP BY SPECIES ORDER BY COUNT(SPECIES) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT AUTHORS.NAME, SUBGENRES.NAME FROM AUTHORS JOIN SERIES ON AUTHORS.ID=SERIES.AUTHOR_ID JOIN 
+  SUBGENRES ON SERIES.subgenre_id=SUBGENRES.ID;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT SERIES.TITLE FROM SERIES JOIN AUTHORS ON SERIES.AUTHOR_ID=AUTHORS.ID JOIN CHARACTERS 
+  ON AUTHORS.ID=CHARACTERS.AUTHOR_ID WHERE CHARACTERS.SPECIES ='human' GROUP BY TITLE ORDER BY COUNT (*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SElECT CHARACTERS.NAME, COUNT (*) FROM CHARACTERS JOIN CHARACTER_books ON 
+  CHARACTERS.ID= CHARACTER_BOOKS.CHARACTER_ID GROUP BY CHARACTERS.NAME ORDER BY COUNT(*) DESC, characters.name;"
 end
--- lib/schema.sql
+++ lib/schema.sql
@@ -1,29 +1,31 @@
 CREATE TABLE series (
-id INTEGER PRIMARY KEY
+id INTEGER PRIMARY KEY,
+title TEXT,
+author_id TEXT,
+subgenre_id TEXT);
 
-);
-
-CREATE TABLE sub_genres (
-id INTEGER PRIMARY KEY
-
-);
+CREATE TABLE subgenres (
+id INTEGER PRIMARY KEY,
+name TEXT);
 
 CREATE TABLE authors (
-id INTEGER PRIMARY KEY
-
-);
+id INTEGER PRIMARY KEY,
+name TEXT);
 
 CREATE TABLE books (
-id INTEGER PRIMARY KEY
-
-);
+id INTEGER PRIMARY KEY,
+title TEXT,
+year INTEGER,
+series_id TEXT);
 
 CREATE TABLE characters (
-id INTEGER PRIMARY KEY
-
-);
+id INTEGER PRIMARY KEY,
+name TEXT,
+species TEXT,
+motto TEXT,
+author_id TEXT);
 
 CREATE TABLE character_books (
-id INTEGER PRIMARY KEY
-
-);
+id INTEGER PRIMARY KEY,
+book_id INTEGER,
+character_id TEXT);
